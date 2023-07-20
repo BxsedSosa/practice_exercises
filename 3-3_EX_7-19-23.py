677848952b1e561f43fb447562ad261fabb2db8d
BxsedSosa <117966396+BxsedSosa@users.noreply.github.com>
2023-07-19 19:34:07 -0700

    Add files via upload

---
+++ 3-3_EX_7-19-23.py
@@ -0,0 +1,14 @@
+#Grading system
+
+g = float(input('What is the percentage?: '))
+d = g * .01
+if d >= .90:
+    print('You got an "A"')
+elif d >= .80:
+    print('You got an "B"')
+elif d >= .70:
+    print('You got an "C"')
+elif d >= .60:
+    print('You got an "D"')
+else:
+    print('You got an "F"')
